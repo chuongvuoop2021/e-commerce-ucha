e5206e88c627bf8a7cc6ef032c1d6a3f604045d8
chuongvuoop2021 <chuongvuoop2021@gmail.com>
2020-02-16 19:25:33 +0200

    Add checkout page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import HomePage from './pages/home-page/HomePage'
 import ShopPage from './pages/shop-page/ShopPage'
 import SignInUp from './pages/sign-in-up/SignInUp'
+import CheckoutPage from './pages/checkout/Checkout'
 import Header from './components/header/Header'
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
+import { selectCurrentUser } from './redux/user/user.selectors'
 
 import './App.css'
 
@@ -45,6 +49,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             exact
             path='/signin'
@@ -62,8 +67,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
